dd2970dee7b541b9ed607ddbc2ed9be6674a4480
kumarmanoj231 <lodhamanoj383@gmail.com>
2025-07-18 03:31:17 +0530

    initial commit

---
+++ backend/src/lib/stream.js
@@ -0,0 +1,33 @@
+import {StreamChat} from "stream-chat"
+import dotenv from "dotenv"
+import { startSession } from "mongoose";
+
+dotenv.config();
+
+const apiKey = process.env.STREAM_API_KEY
+const apiSecret = process.env.STREAM_API_SECRET
+
+if(!apiKey || !apiSecret){
+    console.error("Stream credientials are undefined!");
+}
+
+const streamClient = StreamChat.getInstance(apiKey,apiSecret);
+
+export const upsertSteamUser = async(userData) =>{
+    try{
+        await streamClient.upsertUsers([userData]);
+        return userData;
+    }catch(error){
+        console.error("Error upserting streamlit user",error);
+    }
+};
+
+export const generateStreamToken = (userId) => {
+  try {
+    // ensure userId is a string
+    const userIdStr = userId.toString();
+    return streamClient.createToken(userIdStr);
+  } catch (error) {
+    console.error("Error generating Stream token:", error);
+  }
+};
